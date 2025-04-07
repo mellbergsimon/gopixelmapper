406bdc9b689b166f8edd605638cdff166d8559bc
Simon Mellberg <mellbergsimon@example.com>
2025-04-07 22:49:58 +0200

    added readme

---
+++ gopixelmapper.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/jsimonetti/go-artnet"
+)
+
+func main() {
+	artsubnet := "192.168.11.0/8"
+	_, cidrnet, _ := net.ParseCIDR(artsubnet)
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println("Error getting network interfaces:", err)
+		return
+	}
+
+	var ip net.IP
+	for _, addr := range addrs {
+		ip = addr.(*net.IPNet).IP
+		if cidrnet.Contains(ip) {
+			break
+		}
+	}
+
+	log := artnet.NewDefaultLogger()
+	c := artnet.NewController("controller-1", ip, log)
+	c.Start()
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		c.SendDMXToAddress([512]byte{0x00, 0xff, 0x00, 0xff, 0x00}, artnet.Address{Net: 0, SubUni: 0})
+		time.Sleep(5 * time.Second)
+		c.SendDMXToAddress([512]byte{0xff, 0x00, 0x00, 0xff, 0x00}, artnet.Address{Net: 0, SubUni: 0})
+		time.Sleep(5 * time.Second)
+		c.SendDMXToAddress([512]byte{0x00, 0x00, 0xff, 0xff, 0x00}, artnet.Address{Net: 0, SubUni: 0})
+		time.Sleep(5 * time.Second)
+		c.SendDMXToAddress([512]byte{}, artnet.Address{Net: 0, SubUni: 0})
+		time.Sleep(5 * time.Second)
+	}()
+
+	for {
+		time.Sleep(time.Second)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+gopixelmapper is a NDI to artnet pixelmapper that works on OSX and Linux.
